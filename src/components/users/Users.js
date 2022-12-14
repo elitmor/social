fbeb60c31d8da357b0b69acc6eae174b8b9a1be7
elitmor <elitmor@gmail.com>
2022-12-14 14:44:53 +0200

    componentDidMount

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,39 +1,56 @@
+import axios from "axios";
+import { Component } from "react";
+import avatar from "../../assets/avatar.svg";
 import "./Users.module.css";
 import styles from "./Users.module.css";
 
-const Users = (props) => {
-  return (
-    <div>
-      {props.users.map((user) => (
-        <div key={user.id}>
-          <div className={styles.inner}>
-            <img className={styles.avatar} src={user.photoUrl} alt='avatar' />
-            <div>{user.fullName}</div>
-            <div>{user.status}</div>
-            <div>{user.location.country}</div>
-            <div>{user.location.city}</div>
-            {user.followed ? (
-              <button
-                onClick={() => {
-                  props.unfollow(user.id);
-                }}
-              >
-                Follow
-              </button>
-            ) : (
-              <button
-                onClick={() => {
-                  props.follow(user.id);
-                }}
-              >
-                Unfollow
-              </button>
-            )}
+class Users extends Component {
+  componentDidMount() {
+    axios
+      .get("https://social-network.samuraijs.com/api/1.0/users")
+      .then((response) => {
+        this.props.setUsers(response.data.items);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.users.map((user) => (
+          <div key={user.id}>
+            <div className={styles.inner}>
+              <img
+                className={styles.avatar}
+                src={user.photos.small != null ? user.photos.small : avatar}
+                alt='avatar'
+              />
+              <div>{user.name}</div>
+              <div>{user.status}</div>
+              <div>{"user.location.country"}</div>
+              <div>{"user.location.city"}</div>
+              {user.followed ? (
+                <button
+                  onClick={() => {
+                    this.props.unfollow(user.id);
+                  }}
+                >
+                  Follow
+                </button>
+              ) : (
+                <button
+                  onClick={() => {
+                    this.props.follow(user.id);
+                  }}
+                >
+                  Unfollow
+                </button>
+              )}
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
-  );
-};
+        ))}
+      </div>
+    );
+  }
+}
 
 export default Users;
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -3,46 +3,14 @@ const FOLLOW = "FOLLOW";
 const UNFOLLOW = "UNFOLLOW";
 
 const initialState = {
-  users: [
-    {
-      id: 1,
-      photoUrl: "https://www.svgrepo.com/show/29895/avatar.svg",
-      fullName: "Alexis",
-      status: "I am boss",
-      location: { country: "USA", city: "Texas" },
-      followed: true,
-    },
-    {
-      id: 2,
-      photoUrl: "https://www.svgrepo.com/show/29895/avatar.svg",
-      fullName: "Ksy",
-      status: "I am boss too",
-      location: { country: "Ukraine", city: "Odessa" },
-      followed: true,
-    },
-    {
-      id: 3,
-      photoUrl: "https://www.svgrepo.com/show/29895/avatar.svg",
-      fullName: "Liza",
-      status: "I am boss too",
-      location: { country: "Ukraine", city: "Kiev" },
-      followed: true,
-    },
-    {
-      id: 4,
-      photoUrl: "https://www.svgrepo.com/show/29895/avatar.svg",
-      fullName: "John",
-      status: "I am boss too",
-      location: { country: "England", city: "London" },
-      followed: false,
-    },
-  ],
+  users: [],
 };
 
 const usersReducer = (state = initialState, action) => {
   switch (action.type) {
-    case SET_USERS:
+    case SET_USERS: {
       return { ...state, users: [...state.users, ...action.users] };
+    }
     case FOLLOW:
       return {
         ...state,
@@ -70,7 +38,7 @@ const usersReducer = (state = initialState, action) => {
   }
 };
 
-export const setUsersActionCreator = () => ({ type: SET_USERS });
+export const setUsersActionCreator = (users) => ({ type: SET_USERS, users });
 export const followActionCreator = (userID) => ({ type: FOLLOW, userID });
 export const unfollowActionCreator = (userID) => ({ type: UNFOLLOW, userID });
 
