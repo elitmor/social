b65ea9e5b76279a9258db615f105378f830c6f90
elitmor <elitmor@gmail.com>
2022-12-30 20:55:30 +0200

    useEffect

--- src/components/profile/profileInfo/ProfileStatus/ProfileStatus.js
+++ src/components/profile/profileInfo/ProfileStatus/ProfileStatus.js
@@ -1,8 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const ProfileStatus = (props) => {
   const [editMode, setEditMode] = useState(false);
   const [status, setStatus] = useState(props.status);
+
+  useEffect(() => {
+    setStatus(props.status);
+  }, [props.status]);
 
   const activateEditMode = () => {
     setEditMode(true);
